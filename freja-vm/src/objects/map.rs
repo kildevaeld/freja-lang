ad7f7da30873e45547d0dcffa2709c2a7fbdba43
Rasmus Kildevaeld <rasmuskildevaeld@gmail.com>
2019-05-12 21:47:57 +0200

    maps

---
+++ freja-vm/src/objects/map.rs
@@ -0,0 +1,81 @@
+use super::super::error::RuntimeResult;
+use super::super::value::{Val, Value};
+use super::objects::Closure;
+use super::objects::Native;
+use super::types::Instance;
+use freja_parser::ast::Number;
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::fmt;
+use std::rc::Rc;
+
+#[cfg_attr(feature = "serde_support", derive(Serialize, Deserialize))]
+#[derive(PartialEq, Debug, Clone, Default)]
+pub struct Map {
+    inner: RefCell<HashMap<String, Val>>,
+}
+
+impl Map {
+    pub fn new(inner: HashMap<String, Val>) -> Map {
+        Map {
+            inner: RefCell::new(inner),
+        }
+    }
+    pub fn is_empty(&self) -> bool {
+        true
+    }
+}
+
+impl fmt::Display for Map {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let s = self
+            .inner
+            .borrow()
+            .iter()
+            .map(|m| format!("{}: {}", m.0, m.1))
+            .collect::<Vec<_>>()
+            .join(", ");
+        write!(f, "[{}]", s)
+    }
+}
+
+impl Instance for Map {
+    fn set_field(&self, name: &str, value: Val) -> RuntimeResult<()> {
+        Ok(())
+    }
+
+    fn get_field(&self, name: &str) -> Option<&Val> {
+        println!("name {}", name);
+        None
+    }
+
+    fn find_method(&self, name: &str) -> Option<Rc<Value>> {
+        None
+    }
+
+    fn call_method(&self, name: &str, values: &[Val]) -> Option<RuntimeResult<Value>> {
+        match name {
+            "len" => Some(Ok(Value::Number(Number::Integer(self.inner.borrow().len() as i64)))),
+            // "get" => {
+            //     let idx = if values.is_empty() {
+            //         0
+            //     } else {
+            //         match values.get(0).unwrap().as_ref() {
+            //             Value::Number(Number::Double(d)) => *d as usize,
+            //             Value::Number(Number::Integer(i)) => *i as usize,
+            //             _ => return Some(Err("invalid index".into())),
+            //         }
+            //     };
+
+            //     Some(Ok(self
+            //         .inner
+            //         .borrow()
+            //         .get(idx)
+            //         .map(|m| m.as_ref().clone())
+            //         .or_else(|| Some(Value::Null))
+            //         .unwrap()))
+            // }
+            _ => None,
+        }
+    }
+}
